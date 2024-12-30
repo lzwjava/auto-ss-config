12837fff03c4de921f9c3353e7413efd3dea24e3
Zhiwei Li <lzwjava@gmail.com>
2024-12-31 01:32:56 +0800

    add

--- update.sh
+++ update.sh
@@ -8,3 +8,7 @@ gsutil -h "Cache-Control:no-cache" cp  config.yaml gs://lzwjava1/
 
 gsutil acl ch -u allUsers:R gs://lzwjava1/config.yaml
 
+echo "https://storage.googleapis.com/lzwjava1/ssconfig"
+
+echo "https://storage.googleapis.com/lzwjava1/config.yaml"
+
